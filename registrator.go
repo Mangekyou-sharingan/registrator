af3ffbd5832ee5272abca34fb1513ab86f50c5fe
adenis <adenis@TemplateDebianJessie>
2019-02-12 23:21:42 +0100

    ok

--- registrator.go
+++ registrator.go
@@ -12,7 +12,7 @@ import (
 
 	dockerapi "github.com/fsouza/go-dockerclient"
 	"github.com/gliderlabs/pkg/usage"
-	"github.com/Mangekyou-sharingan/registrator/bridge"
+	"github.com/gliderlabs/registrator/bridge"
 )
 
 var Version string
